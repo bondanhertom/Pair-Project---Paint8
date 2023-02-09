b1f8f8c7c7e0e0f0cbf18b7fade10491351a21f1
Bondan Herutomo <bondanherutomo35@gmail.com>
2023-02-09 21:24:10 +0700

    fitur add done

--- controllers/index.js
+++ controllers/index.js
@@ -21,6 +21,7 @@ class Controller {
         const { error } = req.query
         res.render("login", { error })
     }
+
     static saveLogin(req, res) {
         const { username, password } = req.body
 
@@ -28,12 +29,11 @@ class Controller {
             .then((user) => {
                 if (user) {
                     const isValidPassword = bcrypt.compareSync(password, user.password);//true false
-                    console.log(isValidPassword);
                     if (isValidPassword) {
                         //berhasil login
                         req.session.UserId = user.id //set session di controller login, ngecek seesion di midlewere root
+                        req.session.role = user.role;
                         return res.redirect("/");
-                        //res.send(user)
                     } else {
                         //ketika gagal login password tidak sama
                         const error = "Invalid Username/Password"
@@ -45,16 +45,47 @@ class Controller {
 
     }
 
-    //// HOME ///////
-    static home(req, res) {
-        res.render("home")
-    }
-
-
     ////////////LOG OUT/////////
-    static getLogout(res, req) {
+    static getLogout(req, res) {
         res.send("logout")
     }
+
+    //// HOME ///////
+    static home(req, res) {
+        const { error } = req.query
+        res.render("home", { error })
+
+    }
+    static addPostingan(req, res) {
+        const id = req.params.id;
+        const { error } = req.query;
+        User.findByPk(id)
+            .then((users) => {
+                res.render("addPostingan", { users, error });
+            })
+            .catch((err) => {
+                res.send(err);
+            });
+    }
+
+    static saveAddPosting(req, res) {
+        const { title, caption, imageUrl } = req.body;
+        const id = req.session.UserId;
+        Post.create({ title, caption, imageUrl, UserId: id })
+            .then(() => {
+                res.redirect("/");
+            })
+            .catch((err) => {
+                if (err.name === "SequelizeValidationError") {
+                    const errors = err.errors.map((el) => {
+                        return el.message;
+                    });
+                    res.send(errors);
+                }
+            });
+    }
+
+
 
 }
 
--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,9 @@ router.post("/register/add", Controller.saveRegister)
 //LOGIN
 router.get("/login", Controller.loginForm)
 
+//LOGOUT
+router.get("/logout", Controller.getLogout)
+
 //SESSION MIDLEWARE///
 router.post("/login", Controller.saveLogin)
 router.use(function (req, res, next) {
@@ -24,8 +27,33 @@ router.use(function (req, res, next) {
 //HOME
 router.get("/", Controller.home)
 
-//LOGOUT
-router.get("/logout", Controller.getLogout)
+// <<<<<<<<  ADD    >>>>>>>>>> //
+
+// <<<<<<<<  ADD    >>>>>>>>>> //
+router.use(function (req, res, next) {
+    if (req.session.UserId && req.session.role !== "Creator") {
+        const error = `You do not have permissions to access`;
+        res.redirect(`/?error=${error}`);
+    } else {
+        next();
+    }
+});
+
+// <<<<<<<<<   ADD   >>>>>>>>>> //
+
+// POST UPLOAD
+router.get("/upload", Controller.addPostingan);
+router.post("/upload", Controller.saveAddPosting);
+
+// // DESTROY // EDIT // LIKE
+// router.get("/delete/:id", Controller.totalDislikes);
+// router.get("/edit/:id", Controller.updatePost);
+// router.post("/edit/:id", Controller.saveUpdate);
+// router.get("/likes/:id", Controller.totalLikes);
+
+
+
+
 
 
 
